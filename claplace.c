5b24a721d4cd6f6368e0d9c8ea763695c65026a9
Chris LeBlanc <crleblanc@gmail.com>
2015-07-16 17:32:36 +1200

    fixed an indexing problem causing the C version to scale poorly.  Now slightly better performance than Cython.

--- claplace.c
+++ claplace.c
@@ -1,11 +1,11 @@
-// C implementation of the 2D laplace equation.  Modifies array in-place.
+// C implementation of the 2D laplace equation.  Modifies array in-place. i is the row, j is the column in the array.
 void c_update(double *u, int nx, int ny, double dx2, double dy2) {
     int i, j, elem;
-    for (i=1; i<nx-1; i++) {
-        for (j=1; j<ny-1; j++) {
-            elem = j*ny + i;
-            u[elem] = ((u[elem+1] + u[elem-1]) * dy2 +
-                       (u[elem+nx] + u[elem-nx]) * dx2) / (2*(dx2+dy2));
+    for (i=1; i<ny-1; i++) {
+        for (j=1; j<nx-1; j++) {
+            elem = i*nx + j;
+            u[elem] = ((u[elem+nx] + u[elem-nx]) * dy2 +
+                       (u[elem+1] + u[elem-1]) * dx2) / (2*(dx2+dy2));
         }
     }
 }
